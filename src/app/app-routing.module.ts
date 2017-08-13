3e2a620e34e1bdbb74c5fc461a1e9a144c4de64b
Abel Gaxiola <abelgaxiola@outlook.com>
2017-08-13 08:27:14 -0700

    Add routes for home and canciones. Implement routerLinkActive directive.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', children: [] },
-  {path: 'home', component: HomeComponent},
-  {path:'canciones', component: CancionesComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'canciones', component: CancionesComponent }
 ];
 
 @NgModule({
